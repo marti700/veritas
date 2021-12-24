398d6d50e37ad50257de4d5205623c5a1e977930
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2021-12-24 10:23:01 -0400

    Added Ones function to vectors
    This function produces a vector of a specified lenght filled with ones

--- linearalgebra/vector/vector.go
+++ linearalgebra/vector/vector.go
@@ -10,20 +10,28 @@ type Vector struct {
 }
 
 func NewVector(v []float64) Vector {
-	return Vector {
-		Size : len(v),
-		Data : v,
+	return Vector{
+		Size: len(v),
+		Data: v,
 	}
 }
-
 
 // calculates the dot produt between two vectors
 func (v Vector) DotProduct(v1 Vector) float64 {
 	var sumbuffer []float64
 
 	for i := range v.Data {
-		sumbuffer = append(sumbuffer, v.Data[i] * v1.Data[i])
+		sumbuffer = append(sumbuffer, v.Data[i]*v1.Data[i])
 	}
 
 	return commons.Sum(sumbuffer)
+}
+
+// Creates a vector of the specified lenght which contents are ones
+func Ones(length int) Vector {
+	temp := make([]float64, length)
+	for i := 0; i < length; i++ {
+		temp[i] = 1
+	}
+	return NewVector(temp)
 }
--- linearalgebra/vector/vector_test.go
+++ linearalgebra/vector/vector_test.go
@@ -15,3 +15,27 @@ func TestDotProduct(t *testing.T) {
 		t.Error("answer should be 32: ", ans)
 	}
 }
+
+func TestOnes(t *testing.T) {
+	ans := vector.NewVector([]float64{1,1,1,1,1,1,1,1,1,1})
+	result := vector.Ones(10)
+
+	if !eq(ans, result)  {
+		t.Error("Answer should be ", ans, "but was ",result)
+	}
+}
+
+// TEST UTILS
+
+// Tests if two vectors are equal
+func eq (v1,v2 vector.Vector) bool {
+	if v1.Size != v2.Size {
+		return false
+	}
+	for i := 0; i<v1.Size; i++ {
+		if v1.Data[i] != v2.Data[i] {
+			return false
+		}
+	}
+	return true
+}
