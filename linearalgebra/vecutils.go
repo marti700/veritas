669d4c8d94374949e775bc0100fb9426b2f31343
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2021-12-28 17:41:51 -0400

    Added DotProduct function to vectutils.go

--- linearalgebra/vecutils.go
+++ linearalgebra/vecutils.go
@@ -7,9 +7,43 @@ package linearalgebra
 import "errors"
 
 // returns the number of elements of a vector
+// an error is returned is the provides matrix is not a vector
 func Size(v Matrix) (int, error) {
-	if v.Col > 1 && v.Row > 1 {
+	if !isVector(v) {
 		return 0, errors.New("Matrix is not a vector")
 	}
 	return v.Col * v.Row, nil
+}
+
+// Calculates the dot product of two vectors
+// A error is returned if th matrices are not vectors or if they have different sizes
+func DotProduct(v1, v2 Matrix) (float64, error) {
+	var sum float64
+
+	v1Size, e1 := Size(v1)
+	v2Size, e2 := Size(v2)
+
+	if e1 !=nil || e2 !=nil {
+		return 0, errors.New("one or both of the matrices are not a vectors")
+	}
+
+	if v1Size != v2Size {
+		return 0, errors.New("vectors must be of the same size")
+	}
+
+	for i := range v1.Data {
+		sum += v1.Data[i] * v2.Data[i]
+	}
+
+	return sum, nil
+}
+
+func isVector(v Matrix) bool {
+	if v.Row == 1 && v.Col > 1 {
+		return true
+	} else if v.Row > 1 && v.Col == 1 {
+		return true
+	} else {
+		return false
+	}
 }
--- linearalgebra/vecutils_test.go
+++ linearalgebra/vecutils_test.go
@@ -29,3 +29,67 @@ func TestSize(t *testing.T) {
 		t.Error("The answer should be ", ans2, "but was ", result2)
 	}
 }
+
+func TestDotProductRowCol(t *testing.T) {
+	v1 := linearalgebra.NewMatrix([][]float64{
+		{1, 2, 3, 4, 5, 6},
+	})
+	v2 := linearalgebra.NewMatrix([][]float64{
+		{1},
+		{2},
+		{3},
+		{4},
+		{5},
+		{6},
+	})
+
+	ans := 91.0
+	result, _ := linearalgebra.DotProduct(v1,v2)
+
+	if ans != result {
+		t.Error("The answer should be ", ans, "but was ", result)
+	}
+}
+func TestDotProductRowRow(t *testing.T) {
+	v1 := linearalgebra.NewMatrix([][]float64{
+		{2, 3, 4, 5, 5},
+	})
+	v2 := linearalgebra.NewMatrix([][]float64{
+		{2, 3, 4, 5, 5},
+	})
+
+	ans1 := 79.0
+
+	result1, _ := linearalgebra.DotProduct(v1,v2)
+
+	if ans1 != result1 {
+		t.Error("The answer should be ", ans1, "but was ", result1)
+	}
+}
+func TestDotProductColCol(t *testing.T) {
+	v1 := linearalgebra.NewMatrix([][]float64{
+		{1},
+		{2},
+		{3},
+		{4},
+		{5},
+		{6},
+	})
+
+	v2 := linearalgebra.NewMatrix([][]float64{
+		{1},
+		{2},
+		{3},
+		{4},
+		{5},
+		{30},
+	})
+
+	ans1 := 235.0
+
+	result1, _ := linearalgebra.DotProduct(v1,v2)
+
+	if ans1 != result1 {
+		t.Error("The answer should be ", ans1, "but was ", result1)
+	}
+}
